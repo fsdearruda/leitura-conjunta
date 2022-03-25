f782fd08032dc3f287bb013014da6db6e3a57de4
Fernando Arruda <fernandojs006@gmail.com>
2022-03-25 15:09:44 -0300

    Atualizando useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,10 +2,14 @@ import axios from "axios";
 import useSWR from "swr";
 
 function useFetch<Data = any>(route: string) {
-  const { data, error } = useSWR<Data>(route, async route => {
-    const response = await axios.get(`/api/${route}`);
-    return response.data;
-  });
+  const { data, error } = useSWR<Data>(
+    route,
+    async route => {
+      const response = await axios.get(`/api/${route}`);
+      return response.data;
+    },
+    { refreshInterval: 1000 }
+  );
   return { data, error };
 }
 
