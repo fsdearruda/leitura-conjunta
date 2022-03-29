26f597ce0df7798204ce6bd4c0ef6bf06f6abd6b
Fernando Arruda <fernandojs006@gmail.com>
2022-03-29 16:49:55 -0300

    Ajustes landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from "next";
-import { Text, Flex } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
+import Link from "next/link";
+import { Text, Flex, Button, Icon } from "@chakra-ui/react";
+import { BsTelegram } from "react-icons/bs";
 import Sidebar from "../components/Sidebar";
 const Home: NextPage = () => {
   return (
     <Sidebar>
       <Flex justifyContent="start" alignItems="start" direction="column">
-        <Text lineHeight="5" fontWeight="bold" fontSize="4xl">
+        <Text fontWeight="bold" fontSize="4xl">
           Leitura Conjunta
         </Text>
-        <Text color="pink.400" fontWeight="bold" fontSize="4xl">
-          13° Edição
-        </Text>
-        <Button color="white" bg="pink.500" _hover={{ bg: "pink.600", color: "pink.50" }}>
-          Participe da LC
-        </Button>
+        <Link href="https://t.me/lcmsfisher1" passHref >
+          <a target="_blank">
+            <Button color="white" bg="pink.500" _hover={{ bg: "pink.600", color: "pink.50" }}>
+              Participe da LC <Icon ml={3} as={BsTelegram}></Icon>
+            </Button>
+          </a>
+        </Link>
       </Flex>
     </Sidebar>
   );
