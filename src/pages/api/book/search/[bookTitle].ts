f9fa79e97611c1722c76c8722cac6faf372232bf
Fernando Arruda <fernandojs006@gmail.com>
2022-04-09 23:42:05 -0300

    amigao

--- src/pages/api/book/search/[bookTitle].ts
+++ src/pages/api/book/search/[bookTitle].ts
@@ -19,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   if (!query) return res.status(400).json({ error: true, message: "Book title is required" });
   try {
     let response = await skoobSearch(query);
-    if (response.length < 0) return res.status(404).json({ error: true, message: "Book not found" });
+    if (response.length === 0) return res.status(404).json({ error: true, message: "Book not found" });
     response = await Promise.all(
       response.map(async book => {
         return await getBook(book.id);
