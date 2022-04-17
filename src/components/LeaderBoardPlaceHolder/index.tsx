931d99619c5f7c75289aeeb45854a00cb841da8a
Fernando Arruda <fernandojs006@gmail.com>
2022-04-17 16:38:26 -0300

    quem ta triste não ta mais

--- src/components/LeaderBoardPlaceHolder/index.tsx
+++ src/components/LeaderBoardPlaceHolder/index.tsx
@@ -1,10 +1,10 @@
-import { Flex, Skeleton, SkeletonCircle, useColorMode, Spacer } from "@chakra-ui/react";
+import { Flex, Skeleton, SkeletonCircle, useColorModeValue, Spacer } from "@chakra-ui/react";
 
 const LeaderBoardPlaceHolder = () => {
-  const colorMode = useColorMode();
+  const isDarkMode = useColorModeValue(true, false);
 
   return (
-    <Flex m=".8em" alignContent="flex-start" w="70%" borderRadius="md" p="1em" bg={!colorMode ? "white" : "gray.700"} flexDirection="row" minW="300px">
+    <Flex m=".8em" alignContent="flex-start" w="70%" borderRadius="md" p="1em" bg={isDarkMode ? "white" : "gray.700"} flexDirection="row" minW="300px">
       <SkeletonCircle size="10" /> <Skeleton ml={5} alignSelf="center" height="20px" width="40%" /> <Spacer /> <Skeleton alignSelf="center" height="20px" width="10%" />
     </Flex>
   );
