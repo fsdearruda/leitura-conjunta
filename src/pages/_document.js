ed6616d44a03d5c88f2fff4bbfb01e7a42aa6b27
Fernando Arruda <fernandojs006@gmail.com>
2022-04-18 06:28:12 -0300

    dev/prod home icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/png" href={process.env.NODE_ENV === "production" ? "/static/28x28.png" : "/static/dev.png"} />
-          <link rel="apple-touch-icon" sizes="128x128" href="/static/112x112.png" />
+          <link rel="icon" type="image/png" href={process.env.NODE_ENV === "production" ? "/static/icon.png" : "/static/dev-icon.png"} />
+          <link rel="apple-touch-icon" sizes="128x128" href={process.env.NODE_ENV === "production" ? "/static/home-icon.png" : "/static/dev-home-icon.png"} />
           <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Jost&display=swap" rel="stylesheet" />
         </Head>
