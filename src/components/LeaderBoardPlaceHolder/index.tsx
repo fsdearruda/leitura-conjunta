fa5c6f70fd09403e6652cb4990ae61a1d897cf24
Fernando Arruda <fernandojs006@gmail.com>
2022-04-18 05:16:27 -0300

    ajustando placeholder

--- src/components/LeaderBoardPlaceHolder/index.tsx
+++ src/components/LeaderBoardPlaceHolder/index.tsx
@@ -4,7 +4,7 @@ const LeaderBoardPlaceHolder = () => {
   const isDarkMode = useColorModeValue(true, false);
 
   return (
-    <Flex m=".8em" alignContent="flex-start" w="70%" borderRadius="md" p="1em" bg={isDarkMode ? "white" : "gray.700"} flexDirection="row" minW="300px">
+    <Flex m=".8em" alignContent="flex-start" maxW="1200px" w="100%" borderRadius="md" p="1em" bg={isDarkMode ? "white" : "gray.700"} flexDirection="row" minW="300px">
       <SkeletonCircle size="10" /> <Skeleton ml={5} alignSelf="center" height="20px" width="40%" /> <Spacer /> <Skeleton alignSelf="center" height="20px" width="10%" />
     </Flex>
   );
